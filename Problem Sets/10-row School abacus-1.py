a4c6dfbea651325cc790700c5a7480f9f46ede3c
Eddy <luznww@yahoo.com.tw>
2017-06-03 18:24:49 +0800

    abacus first longer one

---
+++ Problem Sets/10-row School abacus-1.py
@@ -0,0 +1,128 @@
+#########################################################################
+#                 10-row School abacus
+#                         by
+#                      Michael H
+#########################################################################
+#       Description partially extracted from from wikipedia 
+#
+#  Around the world, abaci have been used in pre-schools and elementary
+#
+# In Western countries, a bead frame similar to the Russian abacus but
+# with straight wires and a vertical frame has been common (see image).
+# Helps schools as an aid in teaching the numeral system and arithmetic
+#
+#         |00000*****   |     row factor 1000000000
+#         |00000*****   |     row factor 100000000
+#         |00000*****   |     row factor 10000000 
+#         |00000*****   |     row factor 1000000
+#         |00000*****   |     row factor 100000
+#         |00000*****   |     row factor 10000
+#         |00000*****   |     row factor 1000
+#         |00000****   *|     row factor 100     * 1
+#         |00000***   **|     row factor 10      * 2
+#         |00000**   ***|     row factor 1       * 3
+#                                        -----------    
+#                             Sum                123 
+#
+# Each row represents a different row factor, starting with x1 at the
+# bottom, ascending up to x1000000000 at the top row.     
+######################################################################
+
+# TASK:
+# Define a procedure print_abacus(integer) that takes a positive integer
+# and prints a visual representation (image) of an abacus setup for a 
+# given positive integer value.
+# 
+# Ranking
+# 1 STAR: solved the problem!
+# 2 STARS: 6 < lines <= 9
+# 3 STARS: 3 < lines <= 6
+# 4 STARS: 0 < lines <= 3
+
+def print_abacus(value):
+    
+    lens=len(str(value))
+    i=lens
+    
+    if value ==0 :
+        j = 10
+    else :    
+        j = 10-lens
+        
+    y=0
+    while y < j :
+        print structure(0)
+        y=y+1
+    z=0
+    while z<lens:
+        strvalue=int(str(value)[z:z+1])
+        z=z+1
+        print structure(strvalue)
+
+def structure(num):
+    s='|'
+    j=10-num
+
+    s=s+tran(j,'F')
+    s=s+'   '
+    if j <10 :
+        s=s+tran(num,'L')
+    s=s+'|' 
+    return s
+    
+def tran(num,string):
+    s=''
+    k=0
+    if string =='F' :
+        while k < num and k<5:
+            s=s+'0'
+            k=k+1
+        while k<num :
+            s=s+'*'
+            k=k+1
+    else :        
+        while num >5 :
+            s=s+'0'
+            num=num-1
+        while num <=5 and num >0 :
+            s=s+'*'
+            num=num-1
+    return s   
+
+###  TEST CASES
+print "Abacus showing 0:"
+print_abacus(0)
+#>>>|00000*****   |
+#>>>|00000*****   |
+#>>>|00000*****   |
+#>>>|00000*****   |
+#>>>|00000*****   |
+#>>>|00000*****   |
+#>>>|00000*****   |
+#>>>|00000*****   |
+#>>>|00000*****   |
+#>>>|00000*****   |
+print "Abacus showing 12345678:"
+print_abacus(12345678)
+#>>>|00000*****   |
+#>>>|00000*****   |
+#>>>|00000****   *|
+#>>>|00000***   **|
+#>>>|00000**   ***|
+#>>>|00000*   ****|
+#>>>|00000   *****|
+#>>>|0000   0*****|
+#>>>|000   00*****|
+#>>>|00   000*****|
+print "Abacus showing 1337:"
+print_abacus(1337)
+#>>>|00000*****   |
+#>>>|00000*****   |
+#>>>|00000*****   |
+#>>>|00000*****   |
+#>>>|00000*****   |
+#>>>|00000*****   |
+#>>>|00000****   *|
+#>>>|00000**   ***|
+#>>>|00000**   ***|
+#>>>|000   00*****|    
